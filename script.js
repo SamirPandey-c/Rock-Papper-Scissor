ec816cc43995d16264e2265307e65adc0c867ca7
Samir Pandey <sp177211@gmail.com>
2025-04-12 13:51:05 +0545

    created UI for the user to interact with and tweeked the code to do some DOM manipulation

--- script.js
+++ script.js
@@ -1,73 +1,81 @@
+const icons = document.querySelectorAll("img");
+const result = document.getElementById("result")
+const humanScoreDisplay = document.getElementById("humanScore")
+const computerScoreDisplay = document.getElementById("computerScore")
 
-let humanScore = 0
-let computerScore = 0
+        let humanScore = 0;
+        let computerScore = 0;
+
+        humanScoreDisplay.textContent = humanScore;
+        computerScoreDisplay.textContent = computerScore;
+
+icons.forEach((icon) =>{
+    icon.addEventListener("click", ()=>{
 
 
-
+    
 function playGame(){
-    
-    
-function getComputerChoice(){
-    let i = Math.floor(Math.random()*3)
-    if(i === 0){
-        return "rock"
-    }else if(i === 1){
-        return "papper"
-    }else{
-        return "scissor"
+        function getComputerChoice(){
+            let i = Math.floor(Math.random()*3)
+                if(i === 0){
+                     return "rock"
+                }else if(i === 1){
+                    return "papper"
+                }else{
+                    return "scissor"
     }
 }
 
-function getHumanChoice(){
-    let i = prompt("Select Your Choice 'Rock, Papper, Scissor'")
-    return i;
-}
+
+            let computerSelection = getComputerChoice()
+            let humanSelection = icon.id;
+
+            console.log(humanSelection);
+            console.log(computerSelection);
 
 
-let computerSelection = getComputerChoice()
-let humanSelection = getHumanChoice();
-    
-    
-    function playRound(humanChoice, computerChoice){
-    
-        humanChoice = humanChoice.toLowerCase();
-    
-        if(humanChoice === ""){
-            alert("Enter a valid play!")
+            function playRound(humanChoice, computerChoice){
+
+
+                if(humanChoice === ""){
+                    alert("Enter a valid play!")
+                }
+                else if(computerChoice === "papper" && humanChoice === "rock"){
+                    computerScore++;
+                    result.textContent = "You Loose! Papper beats Rock"
+                    computerScoreDisplay.textContent = computerScore;
+                    
+                }else if(computerChoice === "rock" && humanChoice === "scissor"){
+                    computerScore++;
+                    result.textContent = "You Loose! Rock beats Scissor"
+                    computerScoreDisplay.textContent = computerScore;
+                }else if(computerChoice === "scissor" && humanChoice === "papper"){
+                    computerScore++;
+                    result.textContent = "You Loose! Scissor beats Papper"
+                    computerScoreDisplay.textContent = computerScore;
+                }else if(computerChoice === "rock" && humanChoice === "rock"){
+                    result.textContent = "It's a Draw"
+                }else if(computerChoice === "papper" && humanChoice === "papper"){
+                    result.textContent = "It's a Draw"
+                }else if(computerChoice === "scissor" && humanChoice === "scissor"){
+                    result.textContent = "It's a Draw"
+                }else{
+                    humanScore++ ;
+                    result.textContent = "You Won!"
+                    humanScoreDisplay.textContent = humanScore;
+                }
+            }
+
+            playRound(humanSelection, computerSelection)
+
         }
-        else if(computerChoice === "papper" && humanChoice === "rock"){
-            computerScore++;
-            alert("You Loose Papper beats Rock")
-        }else if(computerChoice === "rock" && humanChoice === "scissor"){
-            computerScore++;
-            alert("You Loose Rock beats Scissor")
-        }else if(computerChoice === "scissor" && humanChoice === "papper"){
-            computerScore++;
-            alert("You Loose Scissor beats Papper")
-        }else if(computerChoice === "rock" && humanChoice === "rock"){
-            alert("It's a Draw!")
-        }else if(computerChoice === "papper" && humanChoice === "papper"){
-            alert("It's a Draw!")
-        }else if(computerChoice === "scissor" && humanChoice === "scissor"){
-            alert("It's a Draw")
-        }else{
-            humanScore++ ;
-            alert("You Won!!!")
-        }
-    }
+
+        
+                playGame();
+        
+
+    })
+})
 
 
 
-
-    playRound(humanSelection, computerSelection)
-
-
-    
-}
-
-    for(let i = 0; i<5; i++){
-  playGame();
-}
-
-
-alert(`Your Score is ${humanScore} Computer Score is ${computerScore}`)
